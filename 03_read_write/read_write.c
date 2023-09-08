93d939945b7e2c4751f461ab4309330788ec309d
Kosma Marcin Hyżorek <kosma.hyzoe@gmail.com>
2023-09-08 23:04:27 +0200

    automated quick commit

--- 03_read_write/read_write.c
+++ 03_read_write/read_write.c
@@ -3,41 +3,64 @@
 #include <linux/fs.h>
 #include <linux/cdev.h>
 #include <linux/uaccess.h>
-#include "../include/hmacros.h"
 
 /* Meta Information */
 MODULE_LICENSE("GPL");
-MODULE_AUTHOR("Kosma Marcin Hyżorek");
-MODULE_DESCRIPTION("A 'yes' equivalent, outputs 'n' on read repeatedly until killed.");
+MODULE_AUTHOR("Johannes 4 GNU/Linux");
+MODULE_DESCRIPTION("Registers a device nr. and implement some callback functions");
 
+/* Buffer for data */
+static char buffer[255];
+static int buffer_pointer = 0;
+
+/* Variables for device and device class */
 static dev_t my_device_nr;
 static struct class *my_class;
 static struct cdev my_device;
 
-static char buffer[255];
-static int buffer_pos = 0;
-static char NO[1] = {'n'};
+#define DRIVER_NAME "dummydriver"
+#define DRIVER_CLASS "MyModuleClass"
 
-#define DRIVER_NAME "no"
-#define DRIVER_CLASS DUMMY_CLASS
-
+/**
+ * @brief Read data out of the buffer
+ */
 static ssize_t driver_read(struct file *File, char *user_buffer, size_t count, loff_t *offs) {
-	if (buffer_pos == 0)
-		return 1 - copy_to_user(user_buffer, NO, 1);
+	int to_copy, not_copied, delta;
 
-	int to_copy = min(count, buffer_pos);
-	int not_copied = copy_to_user(user_buffer, buffer, to_copy);
-	return to_copy - not_copied;
+	/* Get amount of data to copy */
+	to_copy = min(count, buffer_pointer);
+
+	/* Copy data to user */
+	not_copied = copy_to_user(user_buffer, buffer, to_copy);
+
+	/* Calculate data */
+	delta = to_copy - not_copied;
+
+	return delta;
 }
 
+/**
+ * @brief Write data to buffer
+ */
 static ssize_t driver_write(struct file *File, const char *user_buffer, size_t count, loff_t *offs) {
-	int to_copy = min(count, sizeof(buffer));
-	int not_copied = copy_from_user(buffer, user_buffer, to_copy);
-	buffer_pos = to_copy;
+	int to_copy, not_copied, delta;
 
-	return to_copy - not_copied;
+	/* Get amount of data to copy */
+	to_copy = min(count, sizeof(buffer));
+
+	/* Copy data to user */
+	not_copied = copy_from_user(buffer, user_buffer, to_copy);
+	buffer_pointer = to_copy;
+
+	/* Calculate data */
+	delta = to_copy - not_copied;
+
+	return delta;
 }
 
+/**
+ * @brief This function is called, when the device file is opened
+ */
 static int driver_open(struct inode *device_file, struct file *instance) {
 	printk("dev_nr - open was called!\n");
 	return 0;
@@ -59,14 +82,22 @@ static struct file_operations fops = {
 	.write = driver_write
 };
 
-static int __init ModuleInit(void) {
+/**
+ * @brief This function is called, when the module is loaded into the kernel
+ */
+static int __init module_init(void) {
+	int retval;
+	printk("Hello, Kernel!\n");
+
 	/* Allocate a device nr */
 	if( alloc_chrdev_region(&my_device_nr, 0, 1, DRIVER_NAME) < 0) {
-		printk("Device nr. could not be allocated!\n");
+		printk("Device Nr. could not be allocated!\n");
 		return -1;
+	} else {
+		printk("alloc_chrdev_region: %d\n", my_device_nr);
 	}
-	printk("no - device nr. major: %d, minor: %d was registered.\n",
-            GET_MAJOR(my_device_nr), GET_MINOR(my_device_nr));
+	printk("read_write - Device Nr. Major: %d, Minor: %d was registered!\n",
+            my_device_nr >> 20, my_device_nr && 0xfffff);
 
 	/* Create device class */
 	if((my_class = class_create(THIS_MODULE, DRIVER_CLASS)) == NULL) {
@@ -110,7 +141,7 @@ static void __exit ModuleExit(void) {
 	printk("Goodbye, Kernel\n");
 }
 
-module_init(ModuleInit);
+module_init(module_init);
 module_exit(ModuleExit);
 
 
